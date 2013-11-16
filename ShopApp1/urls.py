c805fe69d23923e5fd15162e6125a8cec750c8c5
Chris LeBaron <chrislebaron@gmail.com>
2013-11-15 23:45:38 -0700

    website starting to work

--- ShopApp1/urls.py
+++ ShopApp1/urls.py
@@ -2,10 +2,9 @@ from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
 admin.autodiscover()
-
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'chris.views.index'),
     url(r'^(?P<slug>[\w\-]+)/$', 'chris.views.post'),
-    (r'^accounts/', include('registration.urls')),
+    url(r'^accounts/', include('registration.urls')),
 )
